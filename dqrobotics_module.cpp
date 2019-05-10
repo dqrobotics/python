af70f36fc2bf782a716e25b55dd86313a1e5e8ed
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-05-10 14:57:26 +0900

    Added line_to_line_angle (Close #9)

--- dqrobotics_module.cpp
+++ dqrobotics_module.cpp
@@ -183,6 +183,7 @@ PYBIND11_MODULE(dqrobotics, m) {
     geometry_py.def_static("point_to_line_squared_distance",   &DQ_Geometry::point_to_line_squared_distance,  "Returns the squared distance between a point and a line");
     geometry_py.def_static("point_to_plane_distance",          &DQ_Geometry::point_to_plane_distance,         "Returns the distance between a point and a plane");
     geometry_py.def_static("line_to_line_squared_distance",    &DQ_Geometry::line_to_line_squared_distance,   "Returns the squared distance between two lines");
+    geometry_py.def_static("line_to_line_angle",               &DQ_Geometry::line_to_line_angle,              "Returns the angle between two lines");
 
     /*****************************************************
      *  Robots Kinematic Models
