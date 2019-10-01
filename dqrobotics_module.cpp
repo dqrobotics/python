ac3faa149ee8db623a4f959aceb3452034589ffb
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2019-09-30 20:31:27 -0700

    Added get_chain

--- dqrobotics_module.cpp
+++ dqrobotics_module.cpp
@@ -318,6 +318,7 @@ PYBIND11_MODULE(dqrobotics, m) {
     dqwholebody_py.def("fkm",(DQ (DQ_WholeBody::*)(const VectorXd&) const)&DQ_WholeBody::fkm,"Gets the fkm.");
     dqwholebody_py.def("fkm",(DQ (DQ_WholeBody::*)(const VectorXd&,const int&) const)&DQ_WholeBody::fkm,"Gets the fkm.");
     dqwholebody_py.def("get_dim_configuration_space",&DQ_WholeBody::get_dim_configuration_space,"Gets the dimention of the configuration space");
+    dqwholebody_py.def("get_chain",&DQ_WholeBody::get_chain, "Returns the DQ_Kinematics at a given index of the chain");
     dqwholebody_py.def("pose_jacobian",&DQ_WholeBody::pose_jacobian,"Returns the combined pose Jacobian");
 
     /*****************************************************
