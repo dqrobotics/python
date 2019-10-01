e98e5aaf6090ce88ec7a8ea41e1cc9f685d1f4c5
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2019-09-30 19:22:10 -0700

    Adjusted to scalar gain instead of matrix

--- dqrobotics_module.cpp
+++ dqrobotics_module.cpp
@@ -360,6 +360,7 @@ PYBIND11_MODULE(dqrobotics, m) {
      *  DQ TaskSpacePseudoInverseController
      * **************************************************/
     py::class_<DQ_TaskSpacePseudoInverseController, DQ_KinematicController> dqtaskspacepseudoinversecontroller_py(robot_control,"DQ_TaskSpacePseudoInverseController");
+    dqtaskspacepseudoinversecontroller_py.def(py::init<DQ_Kinematics*>());
     dqtaskspacepseudoinversecontroller_py.def("compute_setpoint_control_signal",&DQ_TaskSpacePseudoInverseController::compute_setpoint_control_signal,"Computes the setpoint control signal.");
     dqtaskspacepseudoinversecontroller_py.def("compute_tracking_control_signal",&DQ_TaskSpacePseudoInverseController::compute_tracking_control_signal,"Computes the tracking control signal.");
 
