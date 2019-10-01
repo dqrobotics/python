f5d30276e585ab0d70f4abe85c28889d2e5d51dd
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2019-09-30 20:15:41 -0700

    Added get_last_error_signal

--- dqrobotics_module.cpp
+++ dqrobotics_module.cpp
@@ -352,6 +352,7 @@ PYBIND11_MODULE(dqrobotics, m) {
     py::class_<DQ_KinematicController> dqkinematiccontroller_py(robot_control,"DQ_KinematicController");
     dqkinematiccontroller_py.def("get_control_objective"  ,&DQ_KinematicController::get_control_objective,"Gets the control objective");
     dqkinematiccontroller_py.def("get_jacobian"           ,&DQ_KinematicController::get_jacobian,"Gets the Jacobian");
+    dqkinematiccontroller_py.def("get_last_error_signal"  ,&DQ_KinematicController::get_last_error_signal, "Gets the last error signal");
     dqkinematiccontroller_py.def("is_set"                 ,&DQ_KinematicController::is_set,"Checks if the controller's objective has been set");
     dqkinematiccontroller_py.def("is_stable"              ,&DQ_KinematicController::is_stable,"Checks if the controller has stabilized");
     dqkinematiccontroller_py.def("set_control_objective"  ,&DQ_KinematicController::set_control_objective,"Sets the control objective");
