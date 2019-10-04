27e873dceeb27f41fb117dc81390f070962e3b87
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-10-04 17:12:36 +0900

    Debugging travis

--- .build.sh
+++ .build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 python3 -m pip install --user twine
 python3 setup.py bdist_wheel
-sudo rm -r build
-pwd
-cd ..
-pwd
 python3 -m pip install --user ./python
 python3 -c "from dqrobotics import *"
-pwd
-cd python
-pwd
--- .deploy.sh
+++ .deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 pwd
+cd python
 ls
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
   then
     cd dist
+    ls
     rename 's/linux/manylinux1/' *
     cd ..
+    ls
     python3 -m twine upload dist/*
 fi
