d042d858a132471633c470efc1246e63264e5a16
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-10-04 17:18:23 +0900

    Debugging travis

--- .build.sh
+++ .build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+pwd
+ls
 python3 -m pip install --user twine
+pwd
+ls
 python3 setup.py bdist_wheel
+pwd
+ls
 python3 -m pip install --user ./python
+pwd
+ls
 python3 -c "from dqrobotics import *"
