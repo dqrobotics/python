648b09c656b9c76151c20c0f5579b1f34f9bf6f1
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-10-04 17:29:43 +0900

    Debugging travis...

--- .build.sh
+++ .build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+echo "Installing twine..."
 python3 -m pip install --user twine
+echo "Compiling wheel..."
 python3 setup.py bdist_wheel
+echo "Deleting build folder..."
+rm -rf build
+echo "Install dqrobotics package..."
 cd ..
 python3 -m pip install --user ./python
 cd python
+echo "Try importing dqrobotics package to check for linking errors..."
 python3 -c "from dqrobotics import *"
