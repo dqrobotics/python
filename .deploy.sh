5cc36175ceffc19cebd403aae2b52e3e4ecd8e03
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-10-04 17:37:44 +0900

    Debugging travis...

--- .deploy.sh
+++ .deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+echo "Starting deployment at..."
 pwd
-cd python
 ls
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
   then
+    echo "Entering dist..."
     cd dist
     ls
+    echo "Renaming wheel..."
     rename 's/linux/manylinux1/' *
+    echo "Leaving dist..."
     cd ..
     ls
+    echo "Uploading to twine..."
     python3 -m twine upload dist/*
 fi
