40db11332c0977b2a804cdecb8b02a1a7612a948
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-10-04 17:46:46 +0900

    Reversed travis, not working well with bionic as os

--- .build.sh
+++ .build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-echo "Installing twine..."
-python3 -m pip install --user twine
-echo "Compiling wheel..."
+
 python3 setup.py bdist_wheel
-echo "Deleting build folder..."
-rm -rf build
-echo "Install dqrobotics package..."
+sudo rm -r build
 cd ..
 python3 -m pip install --user ./python
-cd python
-echo "Try importing dqrobotics package to check for linking errors..."
 python3 -c "from dqrobotics import *"
+cd python
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update -q
+sudo apt-get install python3.6 python3.6-dev -y
+curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6
+python3.6 -m pip install --user setuptools wheel setuptools-git twine
+python3.6 setup.py bdist_wheel
+sudo rm -r build
+cd ..
+python3.6 -m pip install --user ./python
+python3.6 -c "from dqrobotics import *"
--- .deploy.sh
+++ .deploy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-echo "Starting deployment at..."
-pwd
-ls
+
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
   then
-    echo "Entering dist..."
+    cd python
     cd dist
-    ls
-    echo "Renaming wheel..."
     rename 's/linux/manylinux1/' *
-    echo "Leaving dist..."
     cd ..
-    ls
-    echo "Uploading to twine..."
-    python3 -m twine upload dist/*
+    python3.6 -m twine upload dist/*
 fi
