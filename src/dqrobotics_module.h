f54fd7773623b19333c07ac47d8cef4535c94100
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2019-10-04 18:28:40 -0700

    Added LGPLv3 headers, added json11

--- src/dqrobotics_module.h
+++ src/dqrobotics_module.h
@@ -60,6 +60,8 @@ namespace py = pybind11;
 #include <dqrobotics/interfaces/vrep/robots/LBR4pVrepRobot.h>
 #include <dqrobotics/interfaces/vrep/robots/YouBotVrepRobot.h>
 
+#include <dqrobotics/interfaces/json11/DQ_JsonReader.h>
+
 using namespace DQ_robotics;
 using namespace Eigen;
 
@@ -92,5 +94,8 @@ void init_DQ_QuadraticProgrammingSolver_py(py::module& m);
 //dqrobotics/interfaces/vrep
 void init_DQ_VrepInterface_py(py::module& m);
 void init_DQ_VrepRobot_py(py::module& m);
+
+//dqrobotics/interfaces/json11
+void init_DQ_JsonReader_py(py::module& m);
 
 #endif
