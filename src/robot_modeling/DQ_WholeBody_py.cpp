fc111f769bda647c2f3a453777ee368c745859be
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2019-10-10 19:56:55 -0700

    Fixing bugs on DQ_WholeBody pose_jacobian

--- src/robot_modeling/DQ_WholeBody_py.cpp
+++ src/robot_modeling/DQ_WholeBody_py.cpp
@@ -36,6 +36,6 @@ void init_DQ_WholeBody_py(py::module& m)
     dqwholebody_py.def("get_chain",&DQ_WholeBody::get_chain, "Returns the DQ_Kinematics at a given index of the chain");
     dqwholebody_py.def("get_chain_as_serial_manipulator",&DQ_WholeBody::get_chain_as_serial_manipulator, "Returns the DQ_SerialManipulator at a given index of the chain");
     dqwholebody_py.def("get_chain_as_holonomic_base",&DQ_WholeBody::get_chain_as_holonomic_base, "Returns the DQ_HolonomicBase at a given index of the chain");
-    dqwholebody_py.def("pose_jacobian",&DQ_WholeBody::pose_jacobian,"Returns the combined pose Jacobian");
-
+    dqwholebody_py.def("pose_jacobian",(MatrixXd (DQ_WholeBody::*)(const VectorXd&, const int&) const)&DQ_WholeBody::pose_jacobian,"Returns the pose Jacobian");
+    dqwholebody_py.def("pose_jacobian",(MatrixXd (DQ_WholeBody::*)(const VectorXd&) const)&DQ_WholeBody::pose_jacobian,"Returns the pose Jacobian");
 }
