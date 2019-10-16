4eefaa2f588b7626c7272effb60f8df364e6802f
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2019-10-16 10:23:56 +0900

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-echo "Building for Ubuntu 16.04..."
+echo "Building for 16.04 ..."
 python3 setup.py bdist_wheel
 sudo rm -r build
 cd ..
 python3 -m pip install --user ./python
-echo "Ubuntu 16.04 build ended."
-echo "Building for Ubuntu 18.04..."
-echo "Installing Python 3.6..."
+python3 -c "from dqrobotics import *"
 cd python
+echo "16.04 build ended."
+echo "Building for 18.04 ..."
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -q
 sudo apt-get install python3.6 python3.6-dev -y
 curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6
 python3.6 -m pip install --user setuptools wheel setuptools-git twine
-echo "Python 3.6 installed."
+python3.6 setup.py bdist_wheel
+sudo rm -r build
 cd ..
 python3.6 -m pip install --user ./python
-echo "Ubuntu 18.04 build ended."
+python3.6 -c "from dqrobotics import *"
+echo "18.04 build ended."
 echo "Testing..."
 cd python
 cd tests
