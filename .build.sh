af7452f11da3e4e0ca5f6fcd38dab11e3585dc43
Murilo <murilo@nml.t.u-tokyo.ac.jp>
2019-10-16 10:01:35 +0900

    Optimized travis build

--- .build.sh
+++ .build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+echo "Building for Ubuntu 16.04..."
 python3 setup.py bdist_wheel
 sudo rm -r build
 cd ..
 python3 -m pip install --user ./python
-python3 -c "from dqrobotics import *"
+echo "Ubuntu 16.04 build ended."
+echo "Building for Ubuntu 18.04..."
+echo "Installing Python 3.6..."
 cd python
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -q
 sudo apt-get install python3.6 python3.6-dev -y
 curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6
 python3.6 -m pip install --user setuptools wheel setuptools-git twine
-python3.6 setup.py bdist_wheel
-sudo rm -r build
-cd ..
+echo "Python 3.6 installed."
 python3.6 -m pip install --user ./python
-python3.6 -c "from dqrobotics import *"
+echo "Ubuntu 18.04 build ended."
 echo "Testing..."
 cd python
 cd tests
