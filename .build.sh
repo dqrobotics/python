e3e55837c09e9b94baddde247f8434d26c9edf19
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2019-11-12 10:41:50 +0900

    Fixing for 3.8

--- .build.sh
+++ .build.sh
@@ -9,7 +9,7 @@ echo "Building for 18.04 (Bionic)..."
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -q
 sudo apt-get install python3.6 python3.6-dev -y
-curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6
+python3.6 -m pip install --user pip
 python3.6 -m pip install --user setuptools wheel setuptools-git twine
 python3.6 setup.py bdist_wheel
 sudo rm -r build
@@ -30,7 +30,7 @@ echo "Testing ended."
 
 echo "Building for 20.04 (Focal) ..."
 sudo apt-get install python3.8 python3.8-dev -y
-curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.8
+python3.8 -m pip install --user pip
 python3.8 -m pip install --user setuptools wheel setuptools-git twine
 sudo rm -r build
 python3.8 setup.py bdist_wheel
