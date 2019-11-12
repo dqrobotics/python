32f5adc0b3eed018fa66a5e5e906680555b8daeb
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2019-11-12 10:21:38 +0900

    Adding support for Ubuntu 20.04 (Focal)
    According to the Ubuntu dev list
    https://lists.ubuntu.com/archives/ubuntu-devel/2019-October/040834.html
    Ubuntu focal will ship with Python 3.8

--- .build.sh
+++ .build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Building for 16.04 ..."
+echo "Building for 16.04 (Xenial)..."
 python3 setup.py bdist_wheel
 sudo rm -r build
 cd ..
@@ -8,7 +8,8 @@ python3 -m pip install --user ./python
 python3 -c "from dqrobotics import *"
 cd python
 echo "16.04 build ended."
-echo "Building for 18.04 ..."
+
+echo "Building for 18.04 (Bionic)..."
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -q
 sudo apt-get install python3.6 python3.6-dev -y
@@ -29,3 +30,16 @@ python3.6 DQ_test.py
 echo "Testing DQ_Kinematics..."
 python3.6 DQ_Kinematics_test.py
 echo "Testing ended."
+
+echo "Building for 20.04 (Focal) ..."
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update -q
+sudo apt-get install python3.8 python3.8-dev -y
+curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.8
+python3.8 -m pip install --user setuptools wheel setuptools-git twine
+python3.8 setup.py bdist_wheel
+sudo rm -r build
+cd ..
+python3.8 -m pip install --user ./python
+python3.8 -c "from dqrobotics import *"
+echo "20.04 build ended."
