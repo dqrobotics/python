e09a4810eceadce20444676b4416dd2b71397879
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2020-04-22 11:17:11 +0900

    Adjusting deploy script for macos

--- .deploy.sh
+++ .deploy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
-  then
-    cd python
-    cd dist
-    rename 's/linux/manylinux1/' *
-    cd ..
-    python3.6 -m twine upload dist/*
+then
+    if [ "$TRAVIS_OS_NAME" = "osx" ]
+    then
+
+        python3 -m twine upload dist/*
+        
+    else
+    
+        cd python
+        cd dist
+        rename 's/linux/manylinux1/' *
+        cd ..
+        python3.6 -m twine upload dist/*
+    
+    fi
 fi
