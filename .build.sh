5641b97f2498a4efe1bb777c26e84964d88f86cf
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2020-04-25 22:27:02 +0900

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -3,32 +3,34 @@
 if [ "$TRAVIS_OS_NAME" = "osx" ]
 then
 
-    echo "[DQRobotics] Building for macOS"
+    echo "[DQRobotics] Building wheel for macOS"
     python3 setup.py bdist_wheel
     
 else
 
-    echo "[DQRobotics] Building for Ubuntu..."
+    echo "[DQRobotics] Building wheel Ubuntu..."
     python3 --version
     python3 -m pip install setuptools wheel setuptools-git twine
     python3 setup.py bdist_wheel
-    echo "[DQRobotics] Ubuntu build ended."
+    echo "[DQRobotics] Ubuntu wheel build ended."
     
     echo "[DQRobotics] Installing..."
-    cd dist
-    python3 -m pip install *.whl
     cd ..
+    python3 -m pip install ./python
+    cd python
     echo "[DQRobotics] Installed."
     
     echo "[DQRobotics] Testing..."
+    cd tests
     python3 -c "from dqrobotics import *"
     python3 -m pip install scipy quadprog
     echo "[DQRobotics] Testing DQ..."
-    python3 tests/DQ_test.py
+    python3 DQ_test.py
     echo "[DQRobotics] Testing DQ_Kinematics..."
-    python3 tests/DQ_Kinematics_test.py
+    python3 DQ_Kinematics_test.py
     echo "[DQRobotics] Testing cpp_issues..."
-    python3 tests/cpp_issues.py
+    python3 cpp_issues.py
+    cd ..
     echo "[DQRobotics] Tested."
     
 fi
