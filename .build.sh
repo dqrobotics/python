a0d4183d6133095f3cf2ee07b2693f5233bf44c4
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2020-07-09 12:56:12 +0900

    TRAVIS: Trying to make travis fail as expected when tests fail.

--- .build.sh
+++ .build.sh
@@ -23,16 +23,58 @@ else
     
     echo "[DQRobotics] Testing..."
     cd tests
+    
     python3 -c "from dqrobotics import *"
+    if [ $? -eq 0 ]
+    then
+      echo "Successfully imported dqrobotics."
+    else
+      echo "Failed importing dqrobotics"
+      exit 1
+    fi
+    
     python3 -m pip install scipy quadprog
+    
     echo "[DQRobotics] Testing DQ..."
     python3 DQ_test.py
+    if [ $? -eq 0 ]
+    then
+      echo "Successfully tested DQ_test.py"
+    else
+      echo "Failed testing DQ_test.py"
+      exit 1
+    fi
+    
     echo "[DQRobotics] Testing DQ_Kinematics..."
     python3 DQ_Kinematics_test.py
+    if [ $? -eq 0 ]
+    then
+      echo "Successfully tested DQ_Kinematics_test.py"
+    else
+      echo "Failed testing DQ_Kinematics_test.py"
+      exit 1
+    fi
+    
     echo "[DQRobotics] Testing cpp_issues..."
     python3 cpp_issues.py
+    if [ $? -eq 0 ]
+    then
+      echo "Successfully tested cpp_issues.py"
+    else
+      echo "Failed testing cpp_issues.py"
+      exit 1
+    fi
+    
     echo "[DQRobotics] Testing python_issues..."
     python3 python_issues.py
+    if [ $? -eq 0 ]
+    then
+      echo "Successfully tested python_issues.py"
+    else
+      echo "Failed testing python_issues.py"
+      exit 1
+    fi
+    
     cd ..
     echo "[DQRobotics] Tested."
     
